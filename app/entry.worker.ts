191da08f3a4c4d5b9aa88d510e78e4fa258bf3c4
Jürgen Leschner <jldec@users.noreply.github.com>
2025-04-05 19:25:33 -0400

    Initial commit

---
+++ app/entry.worker.ts
@@ -0,0 +1,12 @@
+import { createRequestHandler } from "react-router";
+import * as build from "virtual:react-router/server-build";
+import { CloudflareContext } from "./bindings";
+
+const handler = createRequestHandler(build);
+
+export default {
+  async fetch(request: Request, env: Cloudflare.Env) {
+    let context = new Map([[CloudflareContext, env]]);
+    return await handler(request, context);
+  },
+};
---
+++ app/routes/home.tsx
@@ -0,0 +1,26 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+import { getBindings } from "~/bindings";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export async function loader({ context }: Route.LoaderArgs) {
+  let { MY_KV } = getBindings(context);
+  let value = await MY_KV.get("key");
+
+  if (!value) {
+    await MY_KV.put("key", "value");
+    value = "fallback";
+  }
+
+  return { value };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return <Welcome value={loaderData.value} />;
+}
